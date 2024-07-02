3aa6d6d86bc09e24b238426fc09230e4869f6be5
George Papagapitos <94078156+gpapagapitos@users.noreply.github.com>
2024-07-02 09:31:17 -0500

    create listeners to move left paddle (#4)

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ l_paddle = Paddle((-350, 0))
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
