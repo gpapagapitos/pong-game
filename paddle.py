3119dda71115aecaf08367190b8ba2e3ace7ddb1
gpapagapitos <george.papagapitos@slalom.com>
2024-07-02 09:10:48 -0500

    paddle moves up and down

--- paddle.py
+++ paddle.py
@@ -2,9 +2,12 @@
 
 from turtle import Turtle
 
+MOVEMENT_AMT = 20
+
 
 class Paddle(Turtle):
     """Class representing a paddle"""
+
     def __init__(self):
         super().__init__()
         self.shape("square")
@@ -12,3 +15,13 @@ class Paddle(Turtle):
         self.color("white")
         self.penup()
         self.goto(350, 0)
+
+    def go_up(self):
+        """Method to move the paddle up"""
+        new_y = self.ycor() + MOVEMENT_AMT
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        """Method to move the paddle down"""
+        new_y = self.ycor() - MOVEMENT_AMT
+        self.goto(self.xcor(), new_y)
