6b2a099d6b17a1d4376ad96bf0cdccb4e9cad418
George Papagapitos <94078156+gpapagapitos@users.noreply.github.com>
2024-07-02 09:27:01 -0500

    create two paddles (#3)

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
 
-paddle = Paddle()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(paddle.go_up, "Up")
-screen.onkey(paddle.go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
