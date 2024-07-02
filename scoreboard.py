91921075d17c7c86850bcdc2b0bfb6eabf98c42b
George Papagapitos <94078156+gpapagapitos@users.noreply.github.com>
2024-07-02 14:24:31 -0500

    implement scoring functionality (#10)
    * implement scoring
    
    * add docstrings
    
    * add docstring to scoreboard class

---
+++ scoreboard.py
@@ -0,0 +1,33 @@
+"""Module creating scoreboard and tracking the score"""
+
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    """Class representing a scoreboard"""
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        """Method to update the scoreboard"""
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
+
+    def l_point(self):
+        """Method to increase left player's score by 1 and update scoreboard"""
+        self.l_score += 1
+        self.update_scoreboard()
+
+    def r_point(self):
+        """Method to increase right player's score by 1 and update scoreboard"""
+        self.r_score += 1
+        self.update_scoreboard()
