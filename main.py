1dc29be518ca105e50d36f68be43a0bd17b36e74
George Papagapitos <94078156+gpapagapitos@users.noreply.github.com>
2024-07-02 08:50:02 -0500

    Create paddle (#1)
    * set paddle size
    
    * paddle created and rendering

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 """Pong game"""
 # pylint: disable=C0103
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 
+paddle = Paddle()
 
 screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,14 @@
+"""Module creating the paddle"""
+
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    """Class representing a paddle"""
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        self.penup()
+        self.goto(350, 0)
