75ad7a63b3e8904dd47bc4adb67c7c84d230d59c
gpapagapitos <george.papagapitos@slalom.com>
2024-07-02 13:58:47 -0500

    implement scoring

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from turtle import Screen
 import time
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -15,7 +16,9 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -41,9 +44,11 @@ while game_is_on:
     # Detect when r_paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     # Detect when l_paddle misses
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
