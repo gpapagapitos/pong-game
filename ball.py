9e8da8948d066cf99148c7d65692eab3f1326123
gpapagapitos <george.papagapitos@slalom.com>
2024-07-02 09:54:37 -0500

    create ball class

---
+++ ball.py
@@ -0,0 +1,10 @@
+"""Module creating the ball"""
+
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
