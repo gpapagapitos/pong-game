4077946fc314e41832eb515f64f5f8e2167bc896
gpapagapitos <george.papagapitos@slalom.com>
2024-07-02 12:01:02 -0500

    ball created and moves

--- ball.py
+++ ball.py
@@ -4,7 +4,16 @@ from turtle import Turtle
 
 
 class Ball(Turtle):
+    """Class representing a ball object"""
+
     def __init__(self):
         super().__init__()
-        self.shape("circle")
         self.color("white")
+        self.shape("circle")
+        self.penup()
+
+    def move(self):
+        """Method to move the ball"""
+        new_x = self.xcor() + 10
+        new_y = self.ycor() + 10
+        self.goto(new_x, new_y)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -24,6 +25,8 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 screen.exitonclick()
